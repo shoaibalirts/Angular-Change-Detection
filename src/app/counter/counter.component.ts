ccdc2d5c0c046302d80bcd24207cbe4d2de11dd2
shoaibalirts <shoaibali.rts@gmail.com>
2024-08-31 17:25:51 +0200

    stopped changed detection using runOutsideAngular method on NgZone

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit, inject, NgZone } from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
@@ -9,7 +9,8 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   styleUrl: './counter.component.css',
   imports: [InfoMessageComponent],
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
+  private zone = inject(NgZone);
   count = signal(0);
 
   get debugOutput() {
@@ -17,6 +18,17 @@ export class CounterComponent {
     return 'Counter Component Debug Output';
   }
 
+  ngOnInit() {
+    setTimeout(() => {
+      this.count.set(0);
+    }, 4000);
+
+    this.zone.runOutsideAngular(() => {
+      setTimeout(() => {
+        console.log('Timer expired');
+      }, 4000);
+    });
+  }
   onDecrement() {
     this.count.update((prevCount) => prevCount - 1);
   }
